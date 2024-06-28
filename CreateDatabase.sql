6498530fc53af1b4d91ad46e25157484675eee34
Vinay kumar <0vinayshastri@gmail.com>
2024-06-28 14:08:42 +0530

    Add files via upload

---
+++ CreateDatabase.sql
@@ -0,0 +1,7 @@
+USE MyDatabase;
+GO
+
+INSERT INTO Employees (EmployeeID, FirstName, LastName, Email)
+VALUES (1, 'John', 'Doe', 'john.doe@example.com'),
+       (2, 'Jane', 'Smith', 'jane.smith@example.com');
+GO
---
+++ QueryData.sql
@@ -0,0 +1,5 @@
+USE MyDatabase;
+GO
+
+SELECT TOP 10 * FROM Employees;
+GO
